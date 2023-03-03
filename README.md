e666cbecae2d6034248c276aa797413c35cee503
Rashed Iqbal <dev.rashediqbal@gmail.com>
2023-03-03 12:38:13 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+<p align="center">
+  <img src="https://raw.githubusercontent.com/iqbal-rashed/youtube-ad-blocker-extension/main/manifest_v3/images/icon128.png">
+</p>
+<h1 align="center"> Youtube Ad Blocker Extension </h1>
+<p align="center">
+  <b >Ad-Blocker Extension for YouTube Videos</b>
+</p>
+
+<br>
+
+## Description
+
+The YouTube Ad Blocker extension is available for both Chrome and Firefox browsers. It is designed to block ads on YouTube videos and offers several features that distinguish it from other ad-blocking extensions available in the marketplace. Specifically,
+
+it allows you to:
+
+-   Block YouTube video ads within half a second
+-   Block image ads that appear on the video
+-   Block text overlay ads that appear on YouTube
+
+## Installation
+
+### For Chrome:
+
+-   Clone the repository.
+-   Open Chrome and navigate to chrome://extensions/
+-   Turn on developer mode.
+-   Click "Load Unpacked" and select the "manifest_v3" folder from the cloned repository.
+-   The extension is now installed and will automatically block ads on YouTube videos.
+
+### For Firefox:
+
+-   Open Firefox and navigate to the Firefox Add-ons Marketplace.
+-   Search for "YouTube Ad Blocker" and select the extension from the results.
+-   Click the "Add to Firefox" button to install the extension.
+-   Follow the prompts to confirm the installation, then restart your browser.
+-   Once the browser has restarted, the YouTube Ad Blocker extension will be active and will automatically block ads on any YouTube videos you watch.
---
+++ manifest_v2/content.js
@@ -0,0 +1,71 @@
+window.onload = () => {
+    const targetNode = document.getElementById("movie_player") || document.body;
+    selfObserver(targetNode);
+};
+
+function selfObserver(documentNode) {
+    const observer = new MutationObserver(function () {
+        adFunction();
+    });
+
+    const config = {
+        subtree: true,
+        childList: true,
+    };
+
+    // Start observing
+    observer.observe(documentNode, config);
+}
+
+function adFunction() {
+    const mainDocument = document.getElementsByClassName(
+        "video-ads ytp-ad-module"
+    );
+    const playerOverlay = document.getElementsByClassName(
+        "ytp-ad-player-overlay"
+    );
+    const imageOverlay = document.getElementsByClassName(
+        "ytp-ad-image-overlay"
+    );
+
+    const skipBtn = document.getElementsByClassName(
+        "ytp-ad-skip-button ytp-button"
+    );
+
+    const videoDocument = document.getElementsByClassName(
+        "video-stream html5-main-video"
+    );
+
+    const textOverlay = document.getElementsByClassName("ytp-ad-text-overlay");
+
+    const playerAds = document.getElementById("player-ads");
+
+    function handleSkipBtn() {
+        if (skipBtn.length > 0) {
+            skipBtn[0].click();
+        }
+    }
+
+    if (mainDocument.length > 0) {
+        handleSkipBtn();
+        if (playerOverlay.length > 0) {
+            playerOverlay[0].style.visibility = "hidden";
+            for (let i = 0; i < videoDocument.length; i++) {
+                if (videoDocument[i] && videoDocument[i].duration) {
+                    videoDocument[i].currentTime = videoDocument[i].duration;
+                }
+            }
+            handleSkipBtn();
+        }
+        if (imageOverlay.length > 0) {
+            imageOverlay[0].style.visibility = "hidden";
+        }
+    }
+
+    if (playerAds) {
+        playerAds.style.display = "none";
+    }
+    if (textOverlay.length > 0) {
+        textOverlay[0].style.display = "none";
+    }
+}
